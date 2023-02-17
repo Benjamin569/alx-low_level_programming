350e742b7cb0272841c589ceedad6d68aa1bbd88
myUsername <myemail@gmail.com>
2023-02-16 21:40:21 -0800

    4th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+/**
+ * main - print a line of code using puts
+ *
+ * return:0
+*/
 
-int main(){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
