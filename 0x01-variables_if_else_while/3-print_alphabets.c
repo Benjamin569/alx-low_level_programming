a56dd8f44b87552532fc8d87c4b65bc99e6fce6b
myUsername <myemail@gmail.com>
2023-02-19 06:46:31 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - to print alphabet in lowercase , followed by an uppercse
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUYWXYZ";
+	int i = 0;
+
+	while (i < 52)
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
