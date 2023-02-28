3ea42ffd26275d2774e8eaf7d4b6deeb00face8b
myUsername <myemail@gmail.com>
2023-02-27 19:47:35 -0800

    len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s : point to the string
+ * Return : the lenght of the string
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
