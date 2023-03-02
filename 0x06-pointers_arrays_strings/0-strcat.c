c1686574dcc8cd4484cacb59af9b7d67f07bfa62
myUsername <myemail@gmail.com>
2023-03-01 18:58:45 -0800

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,18 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	while (*dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (*src[i} != '\0')
+	while (src[i} != '\0')
 	{
-		*dest[i] = *src[j};
+		dest[i] = src[j};
 		i++;
 		j++;
 	}
-	*dest[i] = '\0';
+	dest[i] = '\0';
 
-	return (*dest);
+	return (dest);
 }
